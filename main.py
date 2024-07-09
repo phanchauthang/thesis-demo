4fab44216beb64dff5305d87f0b0d0c14da733a6
Thang <meocaoge2@gmail.com>
2024-07-09 20:38:34 +0700

    hehe

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import streamlit as st
-import transformers
 from transformers import pipeline
 
-# Define model names and label dictionaries
-model_names = {
+# Load models and define label dictionaries
+model_names_visogcn = {
     "emotion": "mecoaoge2/VSMEC",
     "sentiment": "mecoaoge2/VLSP",
     "spam": "mecoaoge2/ViSpam",
@@ -11,19 +10,57 @@ model_names = {
     "hate_speech_span": "mecoaoge2/ViHOS"
 }
 
+model_names_visobert = {
+    "emotion": "mecoaoge2/VSMEC1",
+    "sentiment": "mecoaoge2/VLSP1",
+    "spam": "mecoaoge2/ViSpam1",
+    "hate_speech": "mecoaoge2/ViHSD1",
+    "hate_speech_span": "mecoaoge2/ViHOS1"
+}
+
 emotion_labels = {'LABEL_6': 'Other', 'LABEL_4': 'Disgust', 'LABEL_5': 'Enjoyment', 'LABEL_3': 'Anger', 'LABEL_2': 'Surprise', 'LABEL_0': 'Sadness', 'LABEL_1': 'Fear'}
 sentiment_labels = {'LABEL_0': 'Good', 'LABEL_1': 'Normal', 'LABEL_2': 'Bad'}
-spam_labels = {'LABEL_0': 'No Spam', 'LABEL_1': 'Fake Review', 'LABEL_2': 'review on brand only', 'LABEL_3': 'non-review'}
+spam_labels = {'LABEL_0': 'No Spam', 'LABEL_1': 'Fake Review', 'LABEL_2': 'review on brand only','LABEL_3':'non-review'}
 hate_speech_labels = {'LABEL_0': 'Good', 'LABEL_1': 'Offensive', 'LABEL_2': 'Hate Speech'}
 
-def classify_text(text, model_name, task, label_dict=None):
-    print(123)
-    classifier = pipeline(task, model=model_name, device=0)
-    
-    predictions = classifier(text)
-    if label_dict:
-        return [{**prediction, 'label': label_dict[prediction['label']]} for prediction in predictions]
-    return predictions
+# Streamlit app
+# Create two columns
+col1, col2 = st.columns([1, 2])
+
+# Place the image in the first column
+with col1:
+    st.image("a1.png", width=130)
+
+# Place the text in the second column
+with col2:
+    st.markdown("""
+    <div style="color: blue; font-size: 24px;">
+                Khóa Luận Tốt Nghiệp <br>
+        VISOBERT: MÔ HÌNH NGÔN NGỮ CHO TÁC VỤ XỬ LÝ DỮ LIỆU TRUYỀN THÔNG XÃ HỘI TIẾNG VIỆT
+    </div>
+    """, unsafe_allow_html=True)
+
+st.title("DEMO")
+text_input = st.text_area("Enter text:", "")
+
+task = st.selectbox("Chọn một nhiệm vụ:", ["Nhận diện cảm xúc", "Phân tích cảm xúc", "Phát hiện spam", "Phát hiện lời nói căm thù", "Phát hiện khoảng lời nói căm thù"])
+model_type = st.selectbox("Chọn một mô hình:", ["VisoBERT", "VisoGCN"])
+
+if model_type == "VisoBERT":
+    model_names = model_names_visobert
+else:
+    model_names = model_names_visogcn
+device = 'cpu'
+classifier_vihos = pipeline("ner", model=model_names["hate_speech_span"], device=device)
+classifiers = {
+    "emotion": pipeline("text-classification", model=model_names["emotion"], device=device),
+    "sentiment": pipeline("text-classification", model=model_names["sentiment"], device=device),
+    "spam": pipeline("text-classification", model=model_names["spam"], device=-1),
+    "hate_speech": pipeline("text-classification", model=model_names["hate_speech"], device=device)
+}
+
+def classify_text(text, classifier):
+    return classifier(text)
 
 def highlight_tokens(text, results):
     highlighted_text = ""
@@ -50,46 +87,30 @@ def highlight_tokens(text, results):
 
     return highlighted_text
 
-# Streamlit app
-# Create two columns
-col1, col2 = st.columns([1, 2])
-
-# Place the image in the first column
-with col1:
-    st.image("a1.png", width=130)
-
-# Place the text in the second column
-with col2:
-    st.markdown("""
-    <div style="color: blue; font-size: 24px;">
-        VISOBERT: MÔ HÌNH NGÔN NGỮ CHO TÁC VỤ XỬ LÝ DỮ LIỆU TRUYỀN THÔNG XÃ HỘI TIẾNG VIỆT
-    </div>
-    """, unsafe_allow_html=True)
-
-st.title("DEMO")
-text_input = st.text_area("Enter text:", "")
-
-task = st.selectbox("Chọn một nhiệm vụ:", ["Nhận diện cảm xúc", "Phân tích cảm xúc", "Phát hiện spam", "Phát hiện lời nói căm thù", "Phát hiện khoảng lời nói căm thù"])
-model_name = st.selectbox("Chọn một mô hình:", ["VisoGCN","VisoBert"])
-
 if st.button("Classify"):
     if text_input:
         if task == "Phát hiện khoảng lời nói căm thù":
-            results_ner = classify_text(text_input, model_names["hate_speech_span"], "ner")
+            results_ner = classify_text(text_input, classifier_vihos)
             highlighted_text = highlight_tokens(text_input, results_ner)
             st.markdown(highlighted_text, unsafe_allow_html=True)
         else:
             if task == "Nhận diện cảm xúc":
-                results = classify_text(text_input, model_names["emotion"], "text-classification", emotion_labels)
+                results = classify_text(text_input, classifiers["emotion"])
+                label = emotion_labels[results[0]['label']]
+                score = results[0]['score']
             elif task == "Phân tích cảm xúc":
-                results = classify_text(text_input, model_names["sentiment"], "text-classification", sentiment_labels)
+                results = classify_text(text_input, classifiers["sentiment"])
+                label = sentiment_labels[results[0]['label']]
+                score = results[0]['score']
             elif task == "Phát hiện spam":
-                results = classify_text(text_input, model_names["spam"], "text-classification", spam_labels)
+                results = classify_text(text_input, classifiers["spam"])
+                label = spam_labels[results[0]['label']]
+                score = results[0]['score']
             elif task == "Phát hiện lời nói căm thù":
-                results = classify_text(text_input, model_names["hate_speech"], "text-classification", hate_speech_labels)
+                results = classify_text(text_input, classifiers["hate_speech"])
+                label = hate_speech_labels[results[0]['label']]
+                score = results[0]['score']
 
-            label = results[0]['label']
-            score = results[0]['score']
             st.write(f"{task}: {label} (Score: {score:.4f})")
     else:
         st.write("Please enter some text to classify.")
